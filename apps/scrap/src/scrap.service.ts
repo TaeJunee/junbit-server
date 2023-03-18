ab01c96deae07c933d18a48fadcec6d4ac5dc42e
t-junne <108136143+t-junne@users.noreply.github.com>
2023-03-18 18:27:25 +0900

    Delete 오류 수정

--- apps/scrap/src/scrap.service.ts
+++ apps/scrap/src/scrap.service.ts
@@ -10,36 +10,22 @@ export class ScrapService implements OnApplicationBootstrap {
   async onApplicationBootstrap() {
 
     const unitList: HoursType[] = [1, 2, 4, 8, 12, 24]
-    // makeInterval(async () => {
-    //   const date = new Date()
-    //   const baseTime = new Date(
-    //     date.getFullYear(),
-    //     date.getMonth(),
-    //     date.getDate(),
-    //     date.getHours() - 1,
-    //   ).toISOString()
-    //   const ISOBaseTime = new Date(baseTime)
-    //   await this.minuteCandleService.create(60, 3)
-    //   for await (let unit of unitList) {
-    //     await this.minuteCandleService.saveRankData(unit, ISOBaseTime)
-    //   }
-    //   await this.minuteCandleService.delete(ISOBaseTime)
-    //   console.log(`Done at ${ISOBaseTime}`)
-    // })
+    makeInterval(async () => {
+      const date = new Date()
+      const baseTime = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        date.getDate(),
+        date.getHours() - 1,
+      ).toISOString()
+      const ISOBaseTime = new Date(baseTime)
+      await this.minuteCandleService.create(60, 3)
+      for await (let unit of unitList) {
+        await this.minuteCandleService.saveRankData(unit, ISOBaseTime)
+      }
+      await this.minuteCandleService.delete(ISOBaseTime)
+      console.log(`Done at ${ISOBaseTime}`)
+    })
 
-    const date = new Date()
-    const baseTime = new Date(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate(),
-      date.getHours() - 1,
-    ).toISOString()
-    const ISOBaseTime = new Date(baseTime)
-    await this.minuteCandleService.create(60, 3)
-    for await (let unit of unitList) {
-      await this.minuteCandleService.saveRankData(unit, ISOBaseTime)
-    }
-    await this.minuteCandleService.delete(ISOBaseTime)
-    console.log(`Done at ${ISOBaseTime}`)
   }
 }
